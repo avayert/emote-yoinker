7f4d3c9b6a39c1322c29987abaa45b11ea32ff85
Ava Yert <ava@yert.pink>
2023-01-24 17:25:24 +0200

    Add a command to send custom emotes from reactions

--- src/main.py
+++ src/main.py
@@ -18,12 +18,10 @@ tree = discord.app_commands.CommandTree(client)
 cache = {}
 
 
-@tree.context_menu(name='yoink emotes')
-async def yoink_emotes(interaction, message: discord.Message):
-    emojis = CUSTOM_EMOJIS.findall(message.content)
-
-    if not emojis:
-        return await interaction.response.send_message("I couldn't find any emojis to yoink in that message.")
+async def send_emojis(interaction, urls):
+    """
+    Given a list of URLs to discord emojis, send a file of each.
+    """
 
     async def callback(url):
         if url not in cache:
@@ -34,13 +32,37 @@ async def yoink_emotes(interaction, message: discord.Message):
 
         return cache[url]
 
-
-    urls = [discord.PartialEmoji.from_str(emoji).url for emoji in emojis]
     resources = await asyncio.gather(*map(callback, urls))
 
     files = [discord.File(filename='file.png', fp=io.BytesIO(resource)) for resource in resources]
 
     return await interaction.response.send_message(files=files, ephemeral=True)
+
+
+@tree.context_menu(name='yoink emotes')
+async def yoink_emotes(interaction, message: discord.Message):
+    emojis = CUSTOM_EMOJIS.findall(message.content)
+
+    if not emojis:
+        return await interaction.response.send_message("There are no custom emoji in that message.")
+
+    urls = [discord.PartialEmoji.from_str(emoji).url for emoji in emojis]
+
+    return await send_emojis(interaction, urls)
+
+
+@tree.context_menu(name='yoink reactions')
+async def yoink_reactions(interaction, message: discord.Message):
+    if not message.reactions:
+        return await interaction.response.send_message("There are no custom reactions on this message.")
+
+    urls = [
+        reaction.url
+        for reaction in message.reactions
+        if isinstance(reaction.emoji, (discord.Emoji, discord.PartialEmoji))
+    ]
+
+    return await send_emojis(interaction, urls)
 
 
 async def main():
